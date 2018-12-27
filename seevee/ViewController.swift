a5dfcd2dab446f3af2fe503ce0a955fd87d3ff87
Aditya Srinivasan <asrinivasan@cinchapi.com>
2018-12-27 18:26:58 +0530

    Add QR code detection and cup placement

--- seevee/ViewController.swift
+++ seevee/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 import SceneKit
 import ARKit
+import Vision
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    
+    var detectedDataAnchor: ARAnchor?
+    var processing = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,14 +24,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
+        // Set the session's delegate
+        sceneView.session.delegate = self
+        
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,6 +36,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        
+        // Enable horizontal plane detection
+        configuration.planeDetection = .horizontal
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -47,47 +51,139 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let touch = touches.first else { return }
-        let result = sceneView.hitTest(touch.location(in: sceneView), types: [ARHitTestResult.ResultType.featurePoint])
-        guard let hitResult = result.last else { return }
-        let hitTransform = SCNMatrix4(hitResult.worldTransform)
-        let hitVector = SCNVector3Make(hitTransform.m41, hitTransform.m42, hitTransform.m43)
-        createBall(position: hitVector)
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        guard let touch = touches.first else { return }
+//        let result = sceneView.hitTest(touch.location(in: sceneView), types: [ARHitTestResult.ResultType.featurePoint])
+//        guard let hitResult = result.last else { return }
+//        let hitTransform = SCNMatrix4(hitResult.worldTransform)
+//        let hitVector = SCNVector3Make(hitTransform.m41, hitTransform.m42, hitTransform.m43)
+//        createBall(position: hitVector)
+//    }
+//
+//    func createBall(position: SCNVector3) {
+//        let plane = SCNPlane(width: 0.1, height: 0.1)
+//        plane.firstMaterial?.diffuse.contents = UIColor.black
+//        let planeNode = SCNNode(geometry: plane)
+//
+//        let text = SCNText(string: "Hello!", extrusionDepth: 1)
+//        text.firstMaterial?.diffuse.contents = UIColor.white
+//        let textNode = SCNNode(geometry: text)
+//        textNode.position = SCNVector3(x: 0, y: 0, z: 0)
+//        textNode.scale = SCNVector3(x: 0.001, y: 0.001, z: 0.001)
+//        planeNode.addChildNode(textNode)
+//
+//
+//        let billboardConstraint = SCNBillboardConstraint()
+//        billboardConstraint.freeAxes = SCNBillboardAxis.Y
+//        planeNode.constraints = [billboardConstraint]
+//        planeNode.position = position
+//
+//        sceneView.scene.rootNode.addChildNode(planeNode)
+//        sceneView.autoenablesDefaultLighting = true
+//    }
+    
+    // MARK: - ARSessionDelegate
+    
+    public func session(_ session: ARSession, didUpdate frame: ARFrame) {
+        // Only run one Vision request at a time
+        if self.processing {
+            return
+        }
+        
+        self.processing = true
+        
+        // Create a Barcode Detection Request
+        let request = VNDetectBarcodesRequest { (request, error) in
+            
+            // Get the first result out of the results, if there are any
+            if let results = request.results, let result = results.first as? VNBarcodeObservation {
+                
+                // Get the bounding box for the bar code and find the center
+                var rect = result.boundingBox
+                
+                // Flip coordinates
+                rect = rect.applying(CGAffineTransform(scaleX: 1, y: -1))
+                rect = rect.applying(CGAffineTransform(translationX: 0, y: 1))
+                
+                // Get center
+                let center = CGPoint(x: rect.midX, y: rect.midY)
+                
+                // Go back to the main thread
+                DispatchQueue.main.async {
+                    
+                    // Perform a hit test on the ARFrame to find a surface
+                    let hitTestResults = frame.hitTest(center, types: [.featurePoint/*, .estimatedHorizontalPlane, .existingPlane, .existingPlaneUsingExtent*/] )
+                    
+                    // If we have a result, process it
+                    if let hitTestResult = hitTestResults.first {
+                        
+                        // If we already have an anchor, update the position of the attached node
+                        if let detectedDataAnchor = self.detectedDataAnchor,
+                            let node = self.sceneView.node(for: detectedDataAnchor) {
+                            
+                            node.transform = SCNMatrix4(hitTestResult.worldTransform)
+                            
+                        } else {
+                            // Create an anchor. The node will be created in delegate methods
+                            self.detectedDataAnchor = ARAnchor(transform: hitTestResult.worldTransform)
+                            self.sceneView.session.add(anchor: self.detectedDataAnchor!)
+                        }
+                    }
+                    
+                    // Set processing flag off
+                    self.processing = false
+                }
+                
+            } else {
+                // Set processing flag off
+                self.processing = false
+            }
+        }
+        
+        // Process the request in the background
+        DispatchQueue.global(qos: .userInitiated).async {
+            do {
+                // Set it to recognize QR code only
+                request.symbologies = [.QR]
+                
+                // Create a request handler using the captured image from the ARFrame
+                let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: frame.capturedImage,
+                                                                options: [:])
+                // Process the request
+                try imageRequestHandler.perform([request])
+            } catch {
+                
+            }
+        }
     }
     
-    func createBall(position: SCNVector3) {
-        let plane = SCNPlane(width: 0.1, height: 0.1)
-        plane.firstMaterial?.diffuse.contents = UIColor.black
-        let planeNode = SCNNode(geometry: plane)
-        
-        let text = SCNText(string: "Hello!", extrusionDepth: 1)
-        text.firstMaterial?.diffuse.contents = UIColor.white
-        let textNode = SCNNode(geometry: text)
-        textNode.position = SCNVector3(x: 0, y: 0, z: 0)
-        textNode.scale = SCNVector3(x: 0.001, y: 0.001, z: 0.001)
-        planeNode.addChildNode(textNode)
-        
-        
-        let billboardConstraint = SCNBillboardConstraint()
-        billboardConstraint.freeAxes = SCNBillboardAxis.Y
-        planeNode.constraints = [billboardConstraint]
-        planeNode.position = position
-        
-        sceneView.scene.rootNode.addChildNode(planeNode)
-        sceneView.autoenablesDefaultLighting = true
-    }
-
     // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
+        // If this is our anchor, create a node
+        if self.detectedDataAnchor?.identifier == anchor.identifier {
+            
+            // Create a 3D Cup to display
+            guard let virtualObjectScene = SCNScene(named: "cup.scn", inDirectory: "art.scnassets/cup") else {
+                return nil
+            }
+            
+            let wrapperNode = SCNNode()
+            
+            for child in virtualObjectScene.rootNode.childNodes {
+                child.geometry?.firstMaterial?.lightingModel = .physicallyBased
+                child.movabilityHint = .movable
+                wrapperNode.addChildNode(child)
+            }
+            
+            // Set its position based off the anchor
+            wrapperNode.transform = SCNMatrix4(anchor.transform)
+            
+            return wrapperNode
+        }
+        
+        return nil
     }
-*/
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
